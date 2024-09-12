4af70b10cfb732fd13837f1585c46c1af67e8ba7
Mayank Yadav <myadav@qodeleaf.com>
2024-09-12 17:36:26 +0530

    add airbnb base ESLint

---
+++ index.js
@@ -0,0 +1,12 @@
+const x = 10;
+function checkPositive(number) {
+  console.log('msyndk');
+  if (number > 0) {
+    return true;
+  }
+  return false;
+}
+
+const rrt = 203;
+
+console.log(checkPositive(230, x, rrt));
